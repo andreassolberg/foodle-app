a4dd6f48af70431f136a231b662a4d5e151f34ba
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-01-08 13:08:19 +0100

    Reconfigure to run on beta.foodl.org hostname

--- app/etc/config.js
+++ app/etc/config.js
@@ -1,6 +1,6 @@
 {
 	"client_id": "610cbba7-3985-45ae-bc9f-0db0e36f71ad",
-	"redirect_uri": "http://foodle.paas.uninett.no/",
+	"redirect_uri": "https://beta.foodl.org/",
 	"_redirect_uri": "http://127.0.0.1:3001/",
 	"debug": false,
 	"instance": "pilot",
@@ -14,7 +14,5 @@
 	},
 
 	"baseURL"  : "https://foodle.gk.feideconnect.no",
-	"_appURL"  : "https://foodl.org/",
-	"__appURL" : "http://127.0.0.1:3001/",
-	"appURL"   : "http://foodle.paas.uninett.no/"
+	"appURL"   : "https://beta.foodl.org/"
 }
